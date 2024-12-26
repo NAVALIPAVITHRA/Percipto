99f11d374afccc02e64912fbe15106941323b4d3
Prathyarthi <prathyarti@gmail.com>
2024-12-26 12:25:21 +0530

    Fix navbar, assets

--- frontend/src/assets/assets_frontend/assets_frontend.js
+++ frontend/src/assets/assets_frontend/assets_frontend.js
@@ -38,7 +38,7 @@ import Neurologist from './Neurologist.svg'
 import Pediatricians from './Pediatricians.svg'
 
 
-export const assets = {
+export const assets_frontend = {
     appointment_img,
     header_img,
     group_profiles,
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { assets } from '../assets/assets_admin/assets';
+import { assets_frontend } from '../assets/assets_frontend/assets_frontend';
 import { NavLink, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
 
-    const navigate = useNavigate();
-    const [showMenu,setShowMenu] =useState(false)
-    const [token,setToken]=useState(true)
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false)
+  const [token, setToken] = useState(true)
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+      <img className='w-44 cursor-pointer' src={assets.admin_logo} alt="" />
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to="/">
           <li className='py-1'>home</li>
@@ -33,15 +34,13 @@ const Navbar = () => {
       </ul>
       <div className='flex items-center gap-4' >
         {
-            token ? <div>
-                <img src="assets/assets_frontend/profile_pic.jpg" alt="Profile Picture" />
-<img src="assets/assets_frontend/dropdown/dropdown.jpg" alt="Dropdown" />
-
-            :<button onClick={()=>navigate('/login')}className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>create account </button>
-     
+          token ? <div>
+            <img className='w-10' src={assets_frontend.profile_pic} alt="Profile Picture" />
+            <img src={assets_frontend.dropdown_icon} alt="Dropdown" />
+          </div>
+            : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>create account </button>
         }
       </div>
-        
     </div>
   );
 };
