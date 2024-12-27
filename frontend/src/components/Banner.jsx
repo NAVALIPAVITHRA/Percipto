f42090989db402a6c498b97255d5ad5d3daad2c8
NAVALIPAVITHRA <navalipavithra25@gmail.com>
2024-12-27 12:19:58 +0530

    error at appcontext

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
+        <div>
+          /*left*/
+          <p>
+         Book Appointment
+          </p>
+          <p>
+            With 100+ Trusted Doctors
+          </p>
+        </div>
+        <button>Create account</button>
+        <div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
-
+import SpecialMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 const Home = () => {
   return (
     <div>
      <Header />
-
-    
+     <SpecialMenu />
+     <TopDoctors />
+     <Banner />
     </div>
   )
 }
