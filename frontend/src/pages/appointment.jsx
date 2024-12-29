3d195c7ae70e7141715b94d49691a80ac743458b
NAVALIPAVITHRA <navalipavithra25@gmail.com>
2024-12-29 20:16:26 +0530

    error at booking slots

--- frontend/src/pages/appointment.jsx
+++ frontend/src/pages/appointment.jsx
@@ -5,8 +5,12 @@ import { assets_frontend, doctors } from '../assets/assets_frontend/assets_front
 
 const Appointment = () => {
     const { docId } = useParams();
-    const { doctor,currencySymbol } = useContext(AppContext);
+    const { doctor, currencySymbol } = useContext(AppContext);
+    const daysofweek = ['SUN','MON','TUES','WED','THURS','FRI','SAT']
     const [docInfo, setDocInfo] = useState(null);
+    const [docSlots, setDocSlots] = useState([]);
+    const [slotIndex, setSlotIndex] = useState(0);
+    const [slotTime, setSlotTime] = useState('');
 
     const fetchDocInfo = () => {
         const docInfo = doctors.find(doc => doc._id === docId);
@@ -14,9 +18,53 @@ const Appointment = () => {
         console.log(docInfo);
     };
 
+    const getAvailableSlots = async () => {
+        setDocSlots([]);
+
+        let today = new Date();
+        for (let i = 0; i < 7; i++) {
+            let currentDate = new Date(today);
+            currentDate.setDate(today.getDate() + i);
+            let endTime = new Date();
+            endTime.setDate(today.getDate() + i);
+            endTime.setHours(21, 0, 0, 0);
+            if (today.getDate() === currentDate.getDate()) {
+                currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours()+1:10)
+                currentDate.setMinutes(currentDate.getMinutes()>30 ?30 :0)
+            }
+            else{
+                currentDate.setHours(10)
+                currentDate.setMinutes(10)
+            }
+            let timeSlots = []
+            while(currentDate < endTime)
+            {
+                let formattedTime = currentDate.toLocaleTimeString({},{hour :'2-digit',minute:'2-digit'})
+                  timeSlots.push({
+                    datetime :new Date(currentDate),
+                    time : formattedTime
+                  })
+                  currentDate.setMinutes(currentDate.getMinutes()+30)
+
+                }
+                setDocSlots(prev=>([...prev,timeSlots]))
+            // Add slots to the docSlots state
+        }
+    };
+
     useEffect(() => {
         fetchDocInfo();
     }, [doctor, docId]);
+
+    useEffect(() => {
+        if (docInfo) {
+            getAvailableSlots();
+        }
+    }, [docInfo]);
+    useEffect(()=>{
+     console.log(docSlots)
+
+    },[docSlots])
 
     return docInfo && (
         <div className='flex flex-col sm:flex-row gap-4'>
@@ -31,10 +79,27 @@ const Appointment = () => {
                 </div>
                 <div className='mt-4'>
                     <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>About <img className='inline-block w-5 h-5' src={assets_frontend.info_icon} alt="Info Icon" /></p>
-                    <p className='text-sm text-gray-500 max-w-[700px] mt-1' >{docInfo.about}</p>
+                    <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
                 </div>
-                <p>Appointment fee: <span>{currencySymbol}{docInfo.fees}</span>
-                </p>
+                <p className='text-gray-500 font-medium mt-4'>Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span></p>
+            </div>
+            <div className='sm:ml-772 sm:pl-4 mt-4 font-medium text-gray-700'>
+                <p>Booking slots</p>
+                <div>
+                    docSlots.length && docSlots.map((item,index)=>(
+
+                <div key={index}>
+                    <p>{item[0] && dayofweek[item[0].datetime.getDay()]}</p>
+                    <p>{item[0] && item[0].datatime.getDate()}</p>
+
+                </div>
+                
+                 )
+                        
+                    )
+                </div>
+
+
             </div>
         </div>
     );
